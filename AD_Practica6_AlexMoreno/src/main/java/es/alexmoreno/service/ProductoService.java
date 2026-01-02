2093925996251573dfc860b62a588fe4162e2b35
alexm <alex.moreno.sesma@iestubalcain.net>
2026-01-02 11:26:37 +0100

    Repository Producto

---
+++ AD_Practica6_AlexMoreno/src/main/java/es/alexmoreno/service/ProductoService.java
@@ -0,0 +1,8 @@
+package es.alexmoreno.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductoService {
+}
